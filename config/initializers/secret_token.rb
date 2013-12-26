c56cf55c2ad5e3ed0f3e5955089e39886def47e0
Justin-lu <gdjyluxiaoyong@gmail.com>
2013-12-26 10:19:33 +0800

    init app && devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBeansmileCom::Application.config.secret_token = '1e8e47ca5b25f5281a5509d124f1b62d269f3d13179660b417bb61f53f435a679ac1a4cace9258ab58cc95d8b0f3c627d5889491cb4e1aae2f1006b87f70d654'
